7ebae5d9316b1f8ea7eb19151d00df14e13e342c
Michael Birch <michael.birch@aurora.dev>
2022-10-05 22:07:19 +0200

    Convenience functions for deploying the aurora-engine and creating an account

--- tests/src/tests/mod.rs
+++ tests/src/tests/mod.rs
@@ -1,4 +1,6 @@
-use crate::aurora_engine_utils::AuroraEngineRepo;
+use aurora_engine_types::types::{Address, Wei};
+
+use crate::aurora_engine_utils::repo::AuroraEngineRepo;
 
 #[tokio::test]
 async fn test_compile_aurora_engine() {
@@ -6,4 +8,14 @@ async fn test_compile_aurora_engine() {
         .await
         .unwrap();
     assert!(!contract.is_empty());
+}
+
+#[tokio::test]
+async fn test_deploy_aurora_engine() {
+    let engine = crate::aurora_engine_utils::deploy_latest().await.unwrap();
+    let address = Address::decode("000000000000000000000000000000000000000a").unwrap();
+    let balance = Wei::new_u64(123456);
+    engine.mint_account(address, 0, balance).await.unwrap();
+    let view_balance = engine.get_balance(address).await.unwrap();
+    assert_eq!(balance, view_balance);
 }
