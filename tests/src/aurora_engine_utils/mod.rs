12753a6e2761aee76b2786677e8d9dd61edce0ec
Michael Birch <michael.birch@aurora.dev>
2022-10-06 16:11:31 +0200

    Feat(tests): Deploy ERC-20 contract to Aurora Engine

--- tests/src/aurora_engine_utils/mod.rs
+++ tests/src/aurora_engine_utils/mod.rs
@@ -1,13 +1,20 @@
+use aurora_engine::parameters::{
+    CallArgs, FunctionCallArgsV2, SubmitResult, TransactionStatus, ViewCallArgs,
+};
 use aurora_engine_types::{
     types::{Address, Wei},
     U256,
 };
 use workspaces::Contract;
 
+pub mod erc20;
 pub mod repo;
 
 const TESTNET_CHAIN_ID: u64 = 1313161555;
 const MAX_GAS: u64 = 300_000_000_000_000;
+
+/// Newtype for bytes that are meant to be used as the input for an EVM contract.
+pub struct ContractInput(pub Vec<u8>);
 
 pub struct AuroraEngine {
     inner: Contract,
@@ -66,5 +73,77 @@ impl AuroraEngine {
             .view("get_balance", address.as_bytes().to_vec())
             .await?;
         Ok(Wei::new(U256::from_big_endian(&outcome.result)))
+    }
+
+    pub async fn deploy_evm_contract(&self, code: Vec<u8>) -> anyhow::Result<Address> {
+        let outcome = self
+            .inner
+            .call("deploy_code")
+            .args(code)
+            .gas(MAX_GAS)
+            .transact()
+            .await?;
+        let result: SubmitResult = outcome.borsh()?;
+        let address = unwrap_success(result.status).and_then(|bytes| {
+            Address::try_from_slice(&bytes)
+                .map_err(|_| anyhow::Error::msg("Deploy result failed to parse as address"))
+        })?;
+        Ok(address)
+    }
+
+    pub async fn call_evm_contract(
+        &self,
+        address: Address,
+        input: ContractInput,
+        value: Wei,
+    ) -> anyhow::Result<SubmitResult> {
+        let args = CallArgs::V2(FunctionCallArgsV2 {
+            contract: address,
+            value: value.to_bytes(),
+            input: input.0,
+        });
+        let outcome = self
+            .inner
+            .call("call")
+            .args_borsh(args)
+            .gas(MAX_GAS)
+            .transact()
+            .await?;
+        let result = outcome.borsh()?;
+        Ok(result)
+    }
+
+    pub async fn view_evm_contract(
+        &self,
+        contract: Address,
+        input: ContractInput,
+        sender: Option<Address>,
+        value: Wei,
+    ) -> anyhow::Result<TransactionStatus> {
+        let args = ViewCallArgs {
+            sender: sender.unwrap_or_default(),
+            address: contract,
+            amount: value.to_bytes(),
+            input: input.0,
+        };
+        let outcome = self
+            .inner
+            .call("view")
+            .args_borsh(args)
+            .gas(MAX_GAS)
+            .transact()
+            .await?;
+        let result = outcome.borsh()?;
+        Ok(result)
+    }
+}
+
+pub fn unwrap_success(status: TransactionStatus) -> anyhow::Result<Vec<u8>> {
+    match status {
+        TransactionStatus::Succeed(bytes) => Ok(bytes),
+        status => Err(anyhow::Error::msg(format!(
+            "Transaction failed: {:?}",
+            status
+        ))),
     }
 }
