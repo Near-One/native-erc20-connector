a2462cbcbbc4be2f7f1ead610553aa9850f48277
Michael Birch <michael.birch@aurora.dev>
2022-10-08 09:11:22 -0400

    Feat(tests): Deploy ERC-20 contract to Aurora Engine (#7)

---
+++ tests/src/aurora_engine_utils/erc20.rs
@@ -0,0 +1,83 @@
+//! Convenience data-types and functions for deploying/interacting with the OpenZeppelin
+//! ERC-20 contract: https://docs.openzeppelin.com/contracts/4.x/erc20#Presets
+
+use crate::aurora_engine_utils::ContractInput;
+use aurora_engine_types::{types::Address, U256};
+use std::path::Path;
+
+const CONTRACT_NAME: &str = "ERC20PresetMinterPauser";
+
+pub struct Constructor {
+    pub code: Vec<u8>,
+    pub abi: ethabi::Contract,
+}
+
+impl Constructor {
+    pub async fn load() -> anyhow::Result<Self> {
+        let res = Path::new("res");
+        let code_hex =
+            tokio::fs::read_to_string(res.join(format!("{}.bin", CONTRACT_NAME))).await?;
+        let code = hex::decode(code_hex)?;
+        let abi = {
+            let abi_bytes = tokio::fs::read(res.join(format!("{}.abi", CONTRACT_NAME))).await?;
+            serde_json::from_slice(&abi_bytes)?
+        };
+        Ok(Self { code, abi })
+    }
+
+    pub fn deploy_code(&self, name: &str, symbol: &str) -> Vec<u8> {
+        // Unwraps are safe because we statically know there is a constructor and it
+        // takes two strings as input.
+        self.abi
+            .constructor()
+            .unwrap()
+            .encode_input(
+                self.code.clone(),
+                &[
+                    ethabi::Token::String(name.to_string()),
+                    ethabi::Token::String(symbol.to_string()),
+                ],
+            )
+            .unwrap()
+    }
+}
+
+pub struct ERC20 {
+    pub abi: ethabi::Contract,
+    pub address: Address,
+}
+
+impl ERC20 {
+    pub fn mint(&self, recipient: Address, amount: U256) -> ContractInput {
+        let data = self
+            .abi
+            .function("mint")
+            .unwrap()
+            .encode_input(&[
+                ethabi::Token::Address(recipient.raw()),
+                ethabi::Token::Uint(amount),
+            ])
+            .unwrap();
+        ContractInput(data)
+    }
+
+    pub fn balance_of(&self, address: Address) -> ContractInput {
+        let data = self
+            .abi
+            .function("balanceOf")
+            .unwrap()
+            .encode_input(&[ethabi::Token::Address(address.raw())])
+            .unwrap();
+        ContractInput(data)
+    }
+}
+
+pub trait ERC20DeployedAt {
+    fn deployed_at(self, address: Address) -> ERC20;
+}
+
+impl ERC20DeployedAt for ethabi::Contract {
+    fn deployed_at(self, address: Address) -> ERC20 {
+        ERC20 { abi: self, address }
+    }
+}
--- tests/src/tests/mod.rs
+++ tests/src/tests/mod.rs
@@ -1,6 +1,8 @@
-use aurora_engine_types::types::{Address, Wei};
-
-use crate::aurora_engine_utils::repo::AuroraEngineRepo;
+use crate::aurora_engine_utils::{self, erc20, erc20::ERC20DeployedAt, repo::AuroraEngineRepo};
+use aurora_engine_types::{
+    types::{Address, Wei},
+    U256,
+};
 
 #[tokio::test]
 async fn test_compile_aurora_engine() {
@@ -12,10 +14,36 @@ async fn test_compile_aurora_engine() {
 
 #[tokio::test]
 async fn test_deploy_aurora_engine() {
-    let engine = crate::aurora_engine_utils::deploy_latest().await.unwrap();
+    let engine = aurora_engine_utils::deploy_latest().await.unwrap();
     let address = Address::decode("000000000000000000000000000000000000000a").unwrap();
     let balance = Wei::new_u64(123456);
     engine.mint_account(address, 0, balance).await.unwrap();
     let view_balance = engine.get_balance(address).await.unwrap();
     assert_eq!(balance, view_balance);
+}
+
+#[tokio::test]
+async fn test_deploy_erc20() {
+    let engine = aurora_engine_utils::deploy_latest().await.unwrap();
+    let constructor = erc20::Constructor::load().await.unwrap();
+    let address = engine
+        .deploy_evm_contract(constructor.deploy_code("TEST", "AAA"))
+        .await
+        .unwrap();
+    let erc20 = constructor.abi.deployed_at(address);
+    let mint_amount = 7654321.into();
+    let recipient = Address::decode("000000000000000000000000000000000000000a").unwrap();
+    let result = engine
+        .call_evm_contract(address, erc20.mint(recipient, mint_amount), Wei::zero())
+        .await
+        .unwrap();
+    aurora_engine_utils::unwrap_success(result.status).unwrap();
+    let result = engine
+        .view_evm_contract(address, erc20.balance_of(recipient), None, Wei::zero())
+        .await
+        .unwrap();
+    let balance = aurora_engine_utils::unwrap_success(result)
+        .map(|bytes| U256::from_big_endian(&bytes))
+        .unwrap();
+    assert_eq!(balance, mint_amount);
 }
